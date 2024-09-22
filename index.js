80843e4304284e5f557dc7aad8c9710cafe19417
Parritz <masong1508@gmail.com>
2024-09-22 14:07:50 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+import unzipper from "unzipper";
+
+const downloadsFolder = process.env.USERPROFILE + "/Downloads";
+const existingFiles = fs.readdirSync(downloadsFolder);
+const destinationBase = "";
+
+fs.watchFile(downloadsFolder, {interval: 500}, () => {
+    const files = fs.readdirSync(downloadsFolder);
+    for (const file of files) {
+        if (!existingFiles.includes(file) && file.startsWith("Lab") && file.endsWith(".zip")) {
+            const sourcePath = `${downloadsFolder}/${file}`;
+            const destinationPath = destinationBase + `/${file}`;
+
+            fs.renameSync(sourcePath, destinationPath);
+            fs.createReadStream(destinationPath)
+                .pipe(unzipper.Extract({ path: destinationBase }))
+                .on('close', () => {
+                    fs.unlinkSync(destinationPath);
+                    console.log(`Extracted ${file}`);
+                });
+        }
+    }
+});
