4b8e614cc7757e56df73d933d7fbbc6da6be1f03
Parritz <masong1508@gmail.com>
2024-09-22 20:58:58 -0400

    Fix extractor for optional labs (why ciw?)

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import unzipper from "unzipper";
 
 const downloadsFolder = process.env.USERPROFILE + "/Downloads";
 const existingFiles = fs.readdirSync(downloadsFolder);
-const destinationBase = ""; // Directory path goes here
+const destinationBase = ""; // Destination directory path goes here
 
 fs.watchFile(downloadsFolder, {interval: 500}, () => {
     const files = fs.readdirSync(downloadsFolder);
     for (const file of files) {
-        if (!existingFiles.includes(file) && file.startsWith("Lab") && file.endsWith(".zip")) {
+        if (!existingFiles.includes(file) && file.includes("Lab") && file.endsWith(".zip")) {
             const sourcePath = `${downloadsFolder}/${file}`;
             const destinationPath = destinationBase + `/${file}`;
 
